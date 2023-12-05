053c0e7b10b7efdf3b71f49eb15e0e34d36a564b
probeshnath <probeshn@gmail.com>
2023-12-05 18:57:23 +0600

    error handleing

--- src/app.js
+++ src/app.js
@@ -5,11 +5,29 @@ const port = process.env.PORT || 5000;
 
 
 
-
+// check route || basic route
 app.get("/health", (req, res) => {
     res.send("Hi this is home get API")
-  })
-  
-  app.listen(port, () => {
+})
+
+//   
+app.all("*", (req, res, next) => {
+    // res.send("sorry this is not right url")
+    // console.log(req.url)
+    const error = new Error(`The requested url is invalid: ${req.url}`)
+    error.status = 404
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+    console.log("from line 19")
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+
+//   listen and start on localhost the server
+app.listen(port, () => {
     console.log(`localhost connect on ${port}`)
-  })
+})
